62bd3677ce1f03ac287b2040c07dcd50220b2bf5
YJScript <oluwatobiloba.agbola@gmail.com>
2023-03-30 10:50:21 -0700

    Python Network

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Display all HTTP methods the server of a given URL will accept.
-curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
+# Script that takes in a URL and displays all HTTP methods the server will accept
+curl -sI "$1" | grep -i "Allow" | awk -F ": " '{ print $2 }'
